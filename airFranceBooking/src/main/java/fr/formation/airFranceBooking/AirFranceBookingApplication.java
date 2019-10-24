dd6c7a2a31304965c86a0be4311bbbd10d7fe851
Laurent Berthelot <laurent.berthelot1969@gmail.com>
2019-10-24 19:48:09 +0200

    first commit

---
+++ airFranceBooking/src/main/java/fr/formation/airFranceBooking/AirFranceBookingApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.airFranceBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirFranceBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirFranceBookingApplication.class, args);
+	}
+
+}
---
+++ airFranceBooking/src/test/java/fr/formation/airFranceBooking/AirFranceBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.airFranceBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirFranceBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
